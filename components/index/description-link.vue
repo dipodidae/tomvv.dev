cefae1830567195c76d9a65c7d18409a4d7666a9
dpdd <dpdd@squat.net>
2024-04-17 17:24:59 +0200

    Add new components: description-link.vue and link.vue, and configure tailwind.config.js

---
+++ components/index/description-link.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const props = defineProps<{
+  to: string
+}>()
+</script>
+
+<template>
+  <NuxtLink :to="props.to" target="_blank" class="text-stone-700 decoration-pink-200 hover:decoration-pink-400 hover:text-stone-900 underline inline-flex underline-offset-2">
+    <slot />
+  </NuxtLink>
+</template>
---
+++ components/index/link.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+const props = withDefaults(defineProps<{
+  to: string
+  icon?: string
+  target?: string
+}>(), {
+  icon: 'fa-solid:rocket',
+  target: '_self',
+})
+</script>
+
+<template>
+  <NuxtLink :to="props.to" :target="props.target" class="flex items-center p-5 px-10 gap-x-4 bg-slate-50 hover:bg-pink-50 group">
+    <div class="text-3xl text-stone-300 flex items-center group-hover:text-pink-400">
+      <Icon :name="props.icon" />
+    </div>
+    <span class="text-stone-500 group-hover:text-pink-700">
+      <slot />
+    </span>
+  </NuxtLink>
+</template>
